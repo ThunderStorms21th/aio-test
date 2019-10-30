25f4cb0fcba3e594f0a420ab286ea59fc20f62f2
ThunderStorms21th <pinakastorm@gmail.com>
2019-10-30 13:48:22 +0100

    sched: WALT: account cumulative window demand
    Energy cost estimation has been a long lasting challenge for WALT
    because WALT guides CPU frequency based on the CPU utilization of
    previous window.  Consequently it's not possible to know newly
    waking-up task's energy cost until WALT's end of the current window.
    
    The WALT already tracks 'Previous Runnable Sum' (prev_runnable_sum)
    and 'Cumulative Runnable Average' (cr_avg).  They are designed for
    CPU frequency guidance and task placement but unfortunately both
    are not suitable for the energy cost estimation.
    
    It's because using prev_runnable_sum for energy cost calculation would
    make us to account CPU and task's energy solely based on activity in the
    previous window so for example, any task didn't have an activity in the
    previous window will be accounted as a 'zero energy cost' task.
    Energy estimation with cr_avg is what energy_diff() relies on at present.
    However cr_avg can only represent instantaneous picture of energy cost
    thus for example, if a CPU was fully occupied for an entire WALT window
    and became idle just before window boundary, and if there is a wake-up,
    energy_diff() accounts that CPU is a 'zero energy cost' CPU.
    
    As a result, introduce a new accounting unit 'Cumulative Window Demand'.
    The cumulative window demand tracks all the tasks' demands have seen in
    current window which is neither instantaneous nor actual execution time.
    Because task demand represents estimated scaled execution time when the
    task runs a full window, accumulation of all the demands represents
    predicted CPU load at the end of window.
    
    Thus we can estimate CPU's frequency at the end of current WALT window
    with the cumulative window demand.
    
    The use of prev_runnable_sum for the CPU frequency guidance and cr_avg
    for the task placement have not changed and these are going to be used
    for both purpose while this patch aims to add an additional statistics.
    
    Change-Id: I9908c77ead9973a26dea2b36c001c2baf944d4f5
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1319,6 +1319,9 @@ struct task_struct {
 #ifdef CONFIG_SMP
 	struct llist_node wake_entry;
 	int on_cpu;
+#ifdef CONFIG_THREAD_INFO_IN_TASK
+	unsigned int cpu;	/* current CPU */
+#endif
 	struct task_struct *last_wakee;
 	unsigned long wakee_flips;
 	unsigned long wakee_flip_decay_ts;
@@ -1332,6 +1335,16 @@ struct task_struct {
 	const struct sched_class *sched_class;
 	struct sched_entity se;
 	struct sched_rt_entity rt;
+#ifdef CONFIG_SCHED_WALT
+	struct ravg ravg;
+	/*
+	 * 'init_load_pct' represents the initial task load assigned to children
+	 * of this task
+	 */
+	u32 init_load_pct;
+	u64 last_sleep_ts;
+#endif
+
 #ifdef CONFIG_CGROUP_SCHED
 	struct task_group *sched_task_group;
 #endif
